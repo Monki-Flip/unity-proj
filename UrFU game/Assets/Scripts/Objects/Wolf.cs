dd87c75ee1c206384c27038f1c20d4ab9b60f129
AnesVijay <2011andreyka@gmail.com>
2021-11-17 07:10:30 +0500

    Create Wolf.cs

---
+++ UrFU game/Assets/Scripts/Objects/Wolf.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityProj
+{
+    public class Wolf : Animal
+    {
+        //волки должны знать, где олени?
+    }
+}
