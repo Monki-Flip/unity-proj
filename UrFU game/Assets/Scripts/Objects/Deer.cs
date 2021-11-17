da7c62edcae8f21c74bac86ee48a82cf85fd005f
AnesVijay <2011andreyka@gmail.com>
2021-11-17 07:10:12 +0500

    Create Deer.cs

---
+++ UrFU game/Assets/Scripts/Objects/Deer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityProj
+{
+    public class Deer : Animal
+    {
+        // массив свободных клеток?
+        //в каком виде хранить кол-во пожираемой пищи?
+    }
+}
