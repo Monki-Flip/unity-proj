4d73cb4a583a8d6c446c3b3ae9e9efcce1018c01
AnesVijay <2011andreyka@gmail.com>
2021-11-17 08:11:25 +0500

    Create ScoreCounter.cs

---
+++ UrFU game/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityProj
+{
+    public class ScoreCounter : MonoBehaviour
+    {
+
+        public int Count(int score, int add){
+            return score + add;
+        }
+    }
+}
