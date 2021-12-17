d5cee0df5ebd2c03600520bef3a8a3180dd0262d
somesimplecore <alex.itman@mail.ru>
2021-12-17 17:27:02 +0500

    Update Task.cs

--- UrFU game/Assets/Scripts/Task.cs
+++ UrFU game/Assets/Scripts/Task.cs
@@ -4,67 +4,65 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace UnityProj
+
+public class Task : MonoBehaviour
 {
-    public class Task : MonoBehaviour
+    public CellsStack CellsStack;
+    public Score Score;
+    public Tutorial Tutorial;
+
+    public GameObject Button;
+
+    public GameObject FirstCheckMark;
+    public GameObject SecondCheckMark;
+
+    public TMP_Text CellsRewardText;
+    public TMP_Text PoitnsRewardText;
+    public int CellsRewardCount;
+    public int PoitnsRewardCount;
+
+    public bool IsFirstConditionDone;
+    public bool IsSecondConditionDone;
+    public bool IsDone;
+
+
+    public void MarkFirstCondition()
     {
-        public CellsStack CellsStack;
-        public Score Score;
-        public Tutorial Tutorial;
+        IsFirstConditionDone = true;
+        FirstCheckMark.SetActive(true);
+        IsDone = IsFirstConditionDone && IsSecondConditionDone ? true : false;
+    }
 
-        public GameObject Button;
+    public void MarkSecondCondition()
+    {
+        IsSecondConditionDone = true;
+        FirstCheckMark.SetActive(true);
+        IsDone = IsFirstConditionDone && IsSecondConditionDone ? true : false;
+    }
 
-        public GameObject FirstCheckMark;
-        public GameObject SecondCheckMark;
-
-        public TMP_Text CellsRewardText;
-        public TMP_Text PoitnsRewardText;
-        public int CellsRewardCount;
-        public int PoitnsRewardCount;
-
-        public bool IsFirstConditionDone;
-        public bool IsSecondConditionDone;
-        public bool IsDone;
-
-
-        public void MarkFirstCondition()
+    public void MakeButtonActive()
+    {
+        if (IsDone)
         {
-            IsFirstConditionDone = true;
-            FirstCheckMark.SetActive(true);
-            IsDone = IsFirstConditionDone && IsSecondConditionDone ? true : false;
+            Button.GetComponent<Image>().color = new Color(0f, 0f, 0f, 0.5f);
+            Button.GetComponent<Button>().interactable = true;
         }
+    }
 
-        public void MarkSecondCondition()
-        {
-            IsSecondConditionDone = true;
-            FirstCheckMark.SetActive(true);
-            IsDone = IsFirstConditionDone && IsSecondConditionDone ? true : false;
-        }
+    public void GetReward()
+    {
+        Score.Add(PoitnsRewardCount);
+        CellsStack.AddRandomCells(CellsRewardCount);
+        DeleteTask();
+    }
 
-        public void MakeButtonActive()
-        {
-            if (IsDone)
-            {
-                Button.GetComponent<Image>().color = new Color(0f, 0f, 0f, 0.5f);
-                Button.GetComponent<Button>().interactable = true;
-            }
-        }
+    public void DeleteTask()
+    {
+        Destroy(gameObject);
+    }
 
-        public void GetReward()
-        {
-            Score.Add(PoitnsRewardCount);
-            CellsStack.AddRandomCells(CellsRewardCount);
-            DeleteTask();
-        }
-
-        public void DeleteTask()
-        {
-            Destroy(gameObject);
-        }
-
-        public void SetTutorialCurrText(int a)
-        {
-            Tutorial.CurrentIteration = a;
-        }
+    public void SetTutorialCurrText(int a)
+    {
+        Tutorial.CurrentIteration = a;
     }
 }
